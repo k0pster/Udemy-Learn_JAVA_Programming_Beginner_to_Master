c681983bab8f41382accf2292cbf28f5860b2a44
k0pster <39728802+k0pster@users.noreply.github.com>
2023-01-31 16:13:58 +0100

    Add files via upload

---
+++ typesRange/typesRange.java
@@ -0,0 +1,40 @@
+import java.lang.*;
+
+
+class typesRange
+{
+	public static void main(String args[])
+	{
+		System.out.println("Typy Danych i ich zares w JAVA:");
+		System.out.println();
+		System.out.println("Byte:");
+		System.out.println("Min values: "+Byte.MIN_VALUE);
+		System.out.println("Max values: "+Byte.MAX_VALUE);
+		System.out.println("Bytes: "+Byte.BYTES);
+		System.out.println();
+		System.out.println("Short:");
+		System.out.println("Min values: "+Short.MIN_VALUE);
+		System.out.println("Max values: "+Short.MAX_VALUE);
+		System.out.println("Bytes: "+Short.BYTES);
+		System.out.println();
+		System.out.println("Integer:");
+		System.out.println("Min values: "+Integer.MIN_VALUE);
+		System.out.println("Max values: "+Integer.MAX_VALUE);
+		System.out.println("Bytes: "+Integer.BYTES);
+		System.out.println();
+		System.out.println();
+		System.out.println("Long:");
+		System.out.println("Min values: "+Long.MIN_VALUE);
+		System.out.println("Max values: "+Long.MAX_VALUE);
+		System.out.println("Bytes: "+Long.BYTES);
+		System.out.println("Float:");
+		System.out.println("Min values: "+Float.MIN_VALUE);
+		System.out.println("Max values: "+Float.MAX_VALUE);
+		System.out.println("Bytes: "+Float.BYTES);
+		System.out.println();
+		System.out.println("Double:");
+		System.out.println("Min values: "+Double.MIN_VALUE);
+		System.out.println("Max values: "+Double.MAX_VALUE);
+		System.out.println("Bytes: "+Double.BYTES);
+	} 
+}
